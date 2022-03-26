b9215db1b9f64c11793b7dd200c46dd13cb01dcf
TeamAbella060420 <jabella@rscm.com.sa>
2022-03-26 14:14:39 +0300

    Update strings.js

--- js/strings.js
+++ js/strings.js
@@ -1,3 +1,5 @@
 const htmlSnippet = '<h1 class="headline">My Headline</h1>';
-const message = "I'm a programmer!";
-console.log("htmlSnippet");
+const message = 'I\m a programmer!';
+
+const multiline = "Hello, Students. \ Welcome to Javascript Basics. \ I hope you learn a lot!" ;
+console.log("multiline");
